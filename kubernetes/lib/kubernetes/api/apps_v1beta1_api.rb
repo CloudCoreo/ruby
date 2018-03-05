a0924b17814949e4b17831813700feda389dd348
sriv1211 <sriv1211@gmail.com>
2018-03-05 00:56:29 +0000

    Updated content-type for deployment

--- kubernetes/lib/kubernetes/api/apps_v1beta1_api.rb
+++ kubernetes/lib/kubernetes/api/apps_v1beta1_api.rb
@@ -127,7 +127,7 @@ module Kubernetes
       # HTTP header 'Accept' (if needed)
       header_params['Accept'] = @api_client.select_header_accept(['application/json', 'application/yaml', 'application/vnd.kubernetes.protobuf'])
       # HTTP header 'Content-Type'
-      header_params['Content-Type'] = @api_client.select_header_content_type(['*/*'])
+      header_params['Content-Type'] = 'application/json' # @api_client.select_header_content_type(['*/*'])
 
       # form parameters
       form_params = {}
